115e90d85956962016272e5a5dc6a9388f740228
jkooks <97080611+jkooks@users.noreply.github.com>
2023-10-17 07:49:18 -0700

    Update Unpin Track.lua

--- Tracks/Unpin Track.lua
+++ Tracks/Unpin Track.lua
@@ -1,8 +1,19 @@
 --This script causes the pinned track to become unpinned (used in conjunction with Dialogue Pin Track.lua)
 
 
-init_proj = reaper.EnumProjects(-1) --store the initial project
 
-ext_key = "is_continue_" .. tostring(init_proj)
 
-if reaper.HasExtState("pin_track", ext_key) then reaper.DeleteExtState("pin_track", ext_key, true) end
+
+function Main()
+	init_proj = reaper.EnumProjects(-1) --store the initial project
+
+	ext_key = "is_continue_" .. tostring(init_proj)
+
+	if reaper.HasExtState("pin_track", ext_key) then reaper.DeleteExtState("pin_track", ext_key, true) end
+
+	reaper.defer(function() end)
+
+	return true
+end
+
+Main()
